d7bb0d7b74901abcd0f49efec1666c11b4c1b412
Dewa Wono Ichsan <dewa.wono.ichsan@gmail.com>
2024-10-19 06:07:50 +0700

    Add image for menu navigation bar
    Create img element using DOM in nav-menu.js file. Add variable menu1, menu2, menu3 that contain query selector HTML element for div with class menu inside function text. Generate image inside div with class name menu.

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ Use image rule for import image file from JavaScript.
 
 https://bontangpost.id/ini-tiga-warung-nasi-padang-murah-di-bontang-yang-wajib-dicoba/ accessed 16 October 2024.
 
-https://www.astronauts.id/blog/resep-rendang-daging-sapi-untuk-lebaran-gurih-dan-nikmat/ accessed 16 October 2024
+https://www.astronauts.id/blog/resep-rendang-daging-sapi-untuk-lebaran-gurih-dan-nikmat/ accessed 16 October 2024.
 
-https://endeus.tv/resep/pecel-lele accessed 16 October 2024
+https://endeus.tv/resep/pecel-lele accessed 16 October 2024.
+
+https://www.freepik.com/free-photo/american-shrimp-fried-rice-served-with-chili-fish-sauce-thai-food_10401237.htm#query=nasi%20goreng&position=0&from_view=keyword&track=ais_hybrid&uuid=09d69e09-3bd3-4714-b98b-93bc47474bcb acessed 19 October 2024.
--- src/nav-menu.js
+++ src/nav-menu.js
@@ -1,5 +1,6 @@
 import rendangImage from "./asset/image/rendang.jpg"
 import friedLeleImage from "./asset/image/fried-lele.png"
+import friedRiceImage from "./asset/image/fried-rice.jpg"
 
 const navMenu = document.querySelector('nav>button:nth-child(2)')
 
@@ -14,6 +15,9 @@ image1.src = rendangImage
 const image2 = document.createElement('img')
 image2.src = friedLeleImage
 
+const image3 = document.createElement('img')
+image3.src = friedRiceImage
+
 function text(){
     containerContent.textContent = 'Choose Your Food to Buy and Eat'
     containerContent.appendChild(listMenu)
@@ -26,8 +30,13 @@ function text(){
         
     }
 
-    document.querySelector('.menu').append(image1)
-
+    const menu1 = document.querySelector('.menu:first-child')
+    const menu2 = document.querySelector('.menu:nth-child(2)')
+    const menu3 = document.querySelector('.menu:nth-child(3)')
+    
+    menu1.appendChild(image1)
+    menu2.appendChild(image2)
+    menu3.appendChild(image3)
 }
 
 export{navMenu, text}
